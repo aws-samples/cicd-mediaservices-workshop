04752d60b51ca89501cafbda69d0a2ec92c0f7b7
Corneliu Croitoru <cornel.croitoru@gmail.com>
2023-11-29 10:38:00 -0800

    update stack description

--- lib/pipeline/index.ts
+++ lib/pipeline/index.ts
@@ -17,7 +17,8 @@ import { PIPELINE_PROD_MANUAL_APPROVAL } from "../workshop-stacks/config/pipelin
  */
 export class PipelineStack extends Stack {
   constructor(app: App, private mediaStage: MediaServicesStage) {
-    super(app, "workshop-pipeline-stack (uksb-1tupboc33)", {
+    super(app, "workshop-pipeline-stack", {
+      description: 'workshop-pipeline-stack (uksb-1tupboc33)',
       env: {
         region: process.env.CDK_DEFAULT_REGION,
         account: process.env.CDK_DEFAULT_ACCOUNT,
