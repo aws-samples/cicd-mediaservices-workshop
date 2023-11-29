737d318e808e1c53339087cf7a02c72f3767b185
Corneliu Croitoru <cornel.croitoru@gmail.com>
2023-11-29 10:40:42 -0800

    update stack description

--- lib/pipeline/index.ts
+++ lib/pipeline/index.ts
@@ -15,7 +15,7 @@ import { BuildSpec } from "aws-cdk-lib/aws-codebuild";
 export class PipelineStack extends Stack {
   constructor(app: App) {
     super(app, "workshop-pipeline-stack", {
-      description: 'workshop-pipeline-stack (uksb-1tupboc33)',
+      description: 'Workshop pipeline stack (uksb-1tupboc33)',
       env: {
         region: process.env.CDK_DEFAULT_REGION,
         account: process.env.CDK_DEFAULT_ACCOUNT,
